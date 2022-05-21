afb83f20c00bdbd173eb41af7ad38bc32170d86e
Edgar <edgarsargsyan@hotmail.com>
2022-05-20 19:43:16 -0600

    Create a story for Show Component and pass as a prop student string,interviewer object,onEdit and onDelete functions

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import { act } from "@testing-library/react";
 
 
 storiesOf("Button", module)
@@ -147,4 +149,11 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (<Show
+    student={"Lydia Miller-Jones"}
+    interviewer={interviewer}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+  />
+  ));
