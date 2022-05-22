4901a7819c9411b6391b76767f255c24bef84f5c
Edgar <edgarsargsyan@hotmail.com>
2022-05-22 13:10:37 -0600

    Add the controlled component to form input and pass the props to the InterviewerList component

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,54 @@
+import InterviewerList from "components/InterviewerList"
+import Button from "components/Button";
+import React, { useState } from 'react';
+
+
+export default function Form(props) {
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+
+  function reset() {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  function cancel() {
+    reset();
+    props.onCancel();
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={e => {
+          e.preventDefault()
+        }}>
+          <input
+            onChange={(e) => {
+              setStudent(e.target.value)
+            }}
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={() => cancel()}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+} 
