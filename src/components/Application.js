1ed3f4e5650b849936850d14fafbc58581082e20
Edgar <edgarsargsyan@hotmail.com>
2022-05-26 15:18:41 -0600

    Combined all the states to state objects

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,13 +45,23 @@ const appointments = {
   }
 };
 
+
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
   useEffect(() => {
     axios.get("/api/days").then(response => setDays(response.data))
   }, [])
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -63,8 +73,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
