830abc30246acd7dbfc5264dc5310c160a5bfb55
Edgar <edgarsargsyan@hotmail.com>
2022-05-26 14:38:40 -0600

    Create a helper folder for helper functions and implement getAppointmentsForDay function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+
+export function getAppointmentsForDay(state, day) {
+  let appointments = [];
+  const filteredDays = state.days.filter(selectedDay => selectedDay.name === day);
+
+  if (filteredDays.length === 0) {
+    return [];
+  }
+
+  const appointmentsId = filteredDays[0].appointments
+  for (const id of appointmentsId) {
+    appointments.push(state.appointments[id])
+  }
+  return appointments;
+}
+
+
+
