d7a7a88bf7f991c9391e1efab58ef6c85798864e
Edgar <edgarsargsyan@hotmail.com>
2022-05-29 15:29:03 -0600

    Using our useVisualMode hook to determine the mode and show the appropriate cpmponent

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,20 +3,43 @@ import "components/Appointment/styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from './Form';
+import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+console.log(props)
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onEdit={() => console.log("Clicked onEdit")}
+          onDelete={() => console.log("Clicked onDelete")}
         />
-        :
-        <Empty />
-      }
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={()=>back()}
+        />
+      )}
+
+
+
     </article>
   );
 }
