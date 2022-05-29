81248bcd0cf82e5d999d09c57967f1379f0dff6b
Edgar <edgarsargsyan@hotmail.com>
2022-05-29 16:49:59 -0600

    Create getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,6 @@
 
 export function getAppointmentsForDay(state, day) {
-  let appointments = [];
+
   const filteredDays = state.days.filter(selectedDay => selectedDay.name === day);
 
   if (filteredDays.length === 0) {
@@ -8,10 +8,9 @@ export function getAppointmentsForDay(state, day) {
   }
 
   const appointmentsId = filteredDays[0].appointments;
-  for (const id of appointmentsId) {
-    appointments.push(state.appointments[id])
-  }
-  return appointments;
+
+  return appointmentsId.map(id => state.appointments[id]);
+
 }
 
 
@@ -29,4 +28,20 @@ export function getInterview(state, interview) {
   return { student, interviewer };
 }
 
+
+
+
+export function getInterviewersForDay(state, day) {
+
+  const filteredDays = state.days.filter(selectedDay => selectedDay.name === day);
+
+  if (filteredDays.length === 0) {
+    return [];
+  }
+
+  const interviewersId = filteredDays[0].interviewers;
+
+  return interviewersId.map(id => state.interviewers[id]);
+
+}
 
