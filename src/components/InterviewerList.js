2ebc00a27e4d1598415b42556cf4fa128128c446
Edgar <edgarsargsyan@hotmail.com>
2022-06-10 18:08:05 -0600

    import PropTypes and add a code to validate that interviewer props is an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,30 +1,34 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 
-export default function InterviewerList({ interviewers, value, onChange }) {
+
+export default function InterviewerList(props) {
  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
         {
-          interviewers.map((interviewer) => {
+         props.interviewers.map((interviewer) => {
             return (
               <InterviewerListItem
                 key={interviewer.id}
                 name={interviewer.name}
                 avatar={interviewer.avatar}
-                selected={interviewer.id === value}
-                setInterviewer={()=>onChange(interviewer.id)}
+                selected={interviewer.id === props.value}
+                setInterviewer={() => props.onChange(interviewer.id)}
               />
             );
           })
         }
       </ul>
     </section>
-
-
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
