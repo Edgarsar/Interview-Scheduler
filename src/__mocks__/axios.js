5b4a46ed687359b2e080c5162504640bdb5ce1c2
Edgar <edgarsargsyan@hotmail.com>
2022-06-12 18:14:18 -0600

    Add the mock delete request

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -87,7 +87,9 @@ export default {
   }),
   put: jest.fn(() => {
     return Promise.resolve({ status: 204, statusText: "No Content" })
-
+  }),
+  delete: jest.fn(() => {
+    return Promise.resolve({ status: 204, statusText: "No Content" })
   })
 }
 
